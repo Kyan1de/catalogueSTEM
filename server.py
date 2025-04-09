76f0bf683e9a4e3a38e762fbf04416d52467e93f
Kyan1de <k.beau1@outlook.com>
2025-04-08 22:47:32 -0600

    did i test it? no. thats for later me to deal with.

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, redirect, url_for, current_app
+# everything exclusively for the website
+from flask import Flask, render_template, redirect, url_for, current_app, request
 from markupsafe import escape
+
+# for database stuff
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-# for debugging reasons
+# for opening the flask log in a different console window thing
+from multiprocessing import Process, Manager
+import subprocess
 import atexit
 
 #initializing all the flask sqlalchemy stuff
@@ -26,6 +31,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 # initialize the db with the app
 db.init_app(app)
 
+# init later, multi-window shenanigans
+shared_dict = None
+
 with app.app_context():
     db.create_all()
 
@@ -33,7 +41,7 @@ with app.app_context():
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', msg=shared_dict["message"])
 
 @app.route("/db/")
 def lookup():
@@ -42,3 +50,43 @@ def lookup():
         print(e.__dict__)
     return render_template('dbTemplate.html', data=[[entry.name, entry.locationText, f"{entry.available} / {entry.booked}", "button goes here"] for entry in data])
 
+#   handles command-line input. 
+#   this will be the only way of adding things to the database for now 
+# because adding logins and forms, and handling POST requests is a little out-of-scope
+# (this will probably change at some point)
+def CLIHandler(flask_window: subprocess.Popen):
+    while True:
+        userInput = input(" CATA > ")
+        try: 
+            if userInput.lower() == "quit":
+                print("shutting down...")
+                flask_window.kill()
+                quit()
+            elif userInput.lower().startswith("set"):
+                _, key, val = userInput.split(" ")
+                shared_dict[key] = val
+            elif userInput.lower() == "help":
+                pass # do this later
+        except Exception as e:
+            # unhelpful? yes. will I make it better? if I have time. 
+            print("something went wrong, please check your command in the help menu or remove any extrenuous spaces") 
+
+def run_flask(shared_dict):
+    global shared_data
+    shared_data = shared_dict  # Shared data between processes
+    app.run()
+
+if __name__ == "__main__":
+    with Manager() as man:
+
+        shared_dict = man.dict()
+        shared_dict["message"] = "hi hello i hope this works"
+
+        # start the flask server as a "separate" process
+        flask_proc = subprocess.Popen(
+                    ["start", "cmd", "/k", f"python -c \"from __main__ import run_flask; run_flask({shared_dict})\""],
+                    shell=True
+                )
+        atexit.register(flask_proc.kill) # makes sure the site goes down when the main process closes
+        # start the input loop
+        CLIHandler(flask_proc)
